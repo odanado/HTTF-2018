ee3524135acafdd206c13a8f82a702ba776da17e
odan <odan3240@gmail.com>
2018-02-17 17:40:27 +0900

    submit 9876326845, ちょっと上がった(それはそう)

--- main.cpp
+++ main.cpp
@@ -29,9 +29,12 @@ class xor128 {
 int dx[4] = {1, 0, -1, 0};
 int dy[4] = {0, 1, 0, -1};
 constexpr int N = 100;
-array<array<int, N>, N> A;
+using Board = array<array<int, N>, N>;
+Board A;
 auto rng = xor128(42);
-vector<tuple<int, int, int>> outputs;
+using Outputs = vector<tuple<int, int, int>>;
+Outputs outputs;
+using i64 = int64_t;
 
 void input() { rep(i, N) rep(j, N) cin >> A[i][j]; }
 void print() {
@@ -42,16 +45,45 @@ void print() {
         cout << get<2>(e) << endl;
     }
 }
+
+Board make_board(const Outputs &outputs) {
+    Board ret = {};
+    for (auto e : outputs) {
+        int X = get<0>(e), Y = get<1>(e), H = get<2>(e);
+        rep(y, N) rep(x, N) { ret[y][x] += max(0, H - abs(x - X) - abs(y - Y)); }
+    }
+    return ret;
+}
+
+i64 calc_diff(const Board &A, const Board &B) {
+    i64 ret = 0;
+    rep(i, N) rep(j, N) ret += abs(A[i][j] - B[i][j]);
+    return ret;
+}
+
 int main() {
     input();
-
-    rep(i, 1000) {
-        int X = rng() % N;
-        int Y = rng() % N;
-        int H = rng() % N + 1;
-        outputs.push_back(make_tuple(X, Y, H));
+    i64 best = -1;
+    rep(_, 100) {
+        Outputs cand;
+        rep(i, 1000) {
+            int X = rng() % N;
+            int Y = rng() % N;
+            int H = rng() % N + 1;
+            cand.push_back(make_tuple(X, Y, H));
+        }
+        auto B = make_board(cand);
+        auto diff = calc_diff(A, B);
+        if (best == -1 || best > diff) {
+            best = diff;
+            outputs = cand;
+            // cerr << diff << endl;
+        }
     }
     print();
+    auto B = make_board(outputs);
+    auto diff = calc_diff(A, B);
+    cerr << diff << endl;
 
     return 0;
 }
